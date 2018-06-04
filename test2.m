f696fd5aeff791aab38fb0268523894cc7ae0a8f
Aniruddha3395 <ani.shembekar@gmail.com>
2018-06-03 19:40:53 -0700

    1

--- test2.m
+++
@@ -1,7 +0,0 @@
-clc;
-clear;
-close;
-
-fileID = fopen('file.txt','r');
-format = '%d %d %d';
-a = fscanf(fileID,format,[3 Inf])'
--- tp.m
+++
@@ -1,8 +0,0 @@
-clc;
-clear;
-
-i = 120:-1:60;
-for j = 1:size(i,2)
-   fprintf('%d,',i(j)) 
-end
-fprintf('\n')
